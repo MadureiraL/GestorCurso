b2976eafcac1029994f99f2b1623e44e1896ae3c
Levi Madureira <abreumadureira@gmail.com>
2024-10-01 11:19:21 -0300

    up

--- obj/Debug/net8.0/GestorCurso.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestorCurso.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorCurso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cefc2ddff77cc69cde4e7817cb820896ebd5a209")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorCurso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorCurso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
